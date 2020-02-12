1761307d56b1a8fdf98ae43fb8cc59d4bf6760fc
Amy Fouzia <afouzia00@stuy.edu>
2020-02-12 11:36:28 -0500

    test n debug

--- draw.py
+++ draw.py
@@ -10,7 +10,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
 
     #vertical lines
     if (x0 == x1):
-         if(y1 < y0):
+        if(y1 < y0):
             y0, y1 = y1, y0
         for i in range(y0, y1 + 1):
             plot(screen, color, int(x0), int(i))
