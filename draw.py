6604b9c7d568c79f19a0915dda66207a836035c5
Amy <afouzia00@stuy.edu>
2020-02-13 10:51:21 -0500

    change

--- draw.py
+++ draw.py
@@ -6,7 +6,6 @@ def draw_line( x0, y0, x1, y1, screen, color):
     B = -(x1 - x0)
     x = x0
     y = y0
-    m = (float(y1-y0)/float(x1-x0))
 
     #vertical lines
     if (x0 == x1):
@@ -19,7 +18,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
         x0, x1 = x1, x0
         y0, y1 = y1, y0
 
-    
+    m = (float(y1-y0)/float(x1-x0))
     #octant 1/5 / horizontal lines
     elif (0 <= m and m <= 1):
         d1 = (2 * A) + B
