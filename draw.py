81301613c6b0ce833dde5184aeeb36bf527a91ac
Amy <afouzia00@stuy.edu>
2020-02-13 10:48:45 -0500

    change

--- draw.py
+++ draw.py
@@ -15,12 +15,13 @@ def draw_line( x0, y0, x1, y1, screen, color):
         for i in range(y0, y1 + 1):
             plot(screen, color, int(x0), int(i))
 
-    if (x1 < x0):
+    elif (x1 < x0):
         x0, x1 = x1, x0
         y0, y1 = y1, y0
 
+    
     #octant 1/5 / horizontal lines
-    if (0 >= m and m <= 1):
+    elif (0 <= m and m <= 1):
         d1 = (2 * A) + B
 
         while x <= x1:
@@ -33,7 +34,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
             d1 = d1 + (2 * A)
 
     #octant 2/6
-    if (1 < m):
+    elif (1 < m):
         d2 = A + (2 * B)
 
         while y <= y1:
@@ -46,7 +47,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
             d2 = d2 + (2 * B)
 
     #octant 8/4
-    if (-1 > m):
+    elif (-1 > m):
         d3 = A - (2 * B)
 
         while y >= y1:
@@ -59,7 +60,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
             d3 = d3 - (2 * B)
 
     #octant 7/3 / horizontal lines
-    if(m <= 0 and m >= -1):
+    elif(m <= 0 and m >= -1):
         d4 = B + (2 * A)
 
         while x <= x1:
