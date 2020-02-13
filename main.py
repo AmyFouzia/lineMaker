82f4960f7e43b2e7be828a5f3fbb79be2afcfa85
Amy <afouzia00@stuy.edu>
2020-02-13 11:19:43 -0500

    changes

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ s = new_screen()
 c = [ 0, 255, 0 ]
 
 #octants 1 and 5
-draw_line(0, 0, XRES-1, YRES-1, s, c)
-draw_line(0, 0, XRES-1, YRES / 2, s, c)
-draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
+#draw_line(0, 0, XRES-1, YRES-1, s, c)
+#draw_line(0, 0, XRES-1, YRES / 2, s, c)
+#draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
+#WORKS
 
 #octants 8 and 4
 #c[BLUE] = 255;
@@ -17,11 +18,11 @@ draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
 #WORKS
 
 #octants 2 and 6
-#c[RED] = 255;
-#c[GREEN] = 0;
-#c[BLUE] = 0;
-#draw_line(0, 0, XRES/2, YRES-1, s, c);
-#draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
+c[RED] = 255;
+c[GREEN] = 0;
+c[BLUE] = 0;
+draw_line(0, 0, XRES/2, YRES-1, s, c);
+draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
 
 #octants 7 and 3
 #c[BLUE] = 255;
