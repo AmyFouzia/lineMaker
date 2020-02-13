6bceb68784dd424974359ff1f1add97f631b4a8a
Amy <afouzia00@stuy.edu>
2020-02-13 12:19:17 -0500

    changes

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ c[GREEN] = 0;
 c[BLUE] = 0;
 draw_line(0, 0, XRES/2, YRES-1, s, c); #WORKS
 draw_line(XRES-1, YRES-1, XRES/2, 0, s, c); #DOESNT WORK
-#draw_line(499, 499, 250, 0, s, c) m = pos
+#draw_line(499, 499, 250, 0, s, c) m = neg
 
 #octants 7 and 3
 #c[BLUE] = 255;
-#draw_line(0, YRES-1, XRES/2, 0, s, c); #WORKS
+draw_line(0, YRES-1, XRES/2, 0, s, c); #WORKS
 draw_line(XRES-1, 0, XRES/2, YRES-1, s, c); #DOESNT WORK
 #draw_line(499, 0, 250, 499, s, c) m = pos
 print(XRES-1, XRES/2, YRES-1)
