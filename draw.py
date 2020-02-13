55becb3c8d2ca16b0ebe27586f7e2fa7e5d8dc65
Amy <afouzia00@stuy.edu>
2020-02-13 12:33:46 -0500

    changes

--- draw.py
+++ draw.py
@@ -8,7 +8,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
     y = y0
     if (x1-x0 != 0):
         m = (float(y1-y0)/float(x1-x0))
-        
+
     #vertical lines
     if (x0 == x1):
         if(y1 < y0):
@@ -16,12 +16,12 @@ def draw_line( x0, y0, x1, y1, screen, color):
         for i in range(y0, y1 + 1):
             plot(screen, color, int(x0), int(i))
 
-    if (x1 < x0):
+    elif (x1 < x0):
         x0, x1 = x1, x0
         y0, y1 = y1, y0
 
     #octant 1/5 / horizontal lines
-    if (0 <= m and m <= 1):
+    elif (0 <= m and m <= 1):
         d1 = (2 * A) + B
 
         while x <= x1:
@@ -34,7 +34,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
             d1 = d1 + (2 * A)
 
     #octant 2/6
-    if (1 < m):
+    elif (1 < m):
         d2 = A + (2 * B)
 
         while y <= y1:
@@ -47,7 +47,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
             d2 = d2 + (2 * B)
 
     #octant 8/4
-    if (-1 > m):
+    elif (-1 > m):
         d3 = A - (2 * B)
 
         while y >= y1:
@@ -60,7 +60,7 @@ def draw_line( x0, y0, x1, y1, screen, color):
             d3 = d3 - (2 * B)
 
     #octant 7/3 / horizontal lines
-    if(m <= 0 and m >= -1):
+    elif(m <= 0 and m >= -1):
         d4 = B + (2 * A)
 
         while x <= x1:
